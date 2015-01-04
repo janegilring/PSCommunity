447813b27c20ba5f4f6fd8cb60c03758de085997
Jan Egil Ring <jan.egil.ring@crayon.com>
2015-01-04 01:40:32 +0100

    Added Update-RoyalFolder.ps1 - an example script created for an article about the Royal TS PowerShell module written for PowerShell Magazine

---
+++ Royal TS/Update-RoyalFolder.ps1
@@ -0,0 +1,337 @@
+﻿<#
+.Synopsis
+	This script will mirror the OU structure from the specified root OU to a top level folder in the specified Royal TS document and create Remote Desktop Connection objects for all AD computer accounts meeting specific criterias.
+
+.Description
+	This script will mirror the OU structure from the specified root OU to a top level folder in the specified Royal TS document and create Remote Desktop Connection objects for all AD computer accounts meeting specific criterias.
+
+    The criterias is the following:
+    -The computer object is registered with a Server operating system (the object`s "Operatingsystem" LDAP property meets the filter "Windows Server*")
+    -The computer object is not a Cluster Name Object (the object`s "ServicePrincipalName" LDAP property does not contain the word MSClusterVirtualServer)
+    -The computer account has logged on to the domain in the past X number of days (X is 60 days if the parameter InactiveComputerObjectThresholdInDays is not specified)
+    
+    The purpose of this script is to show how the Royal TS PowerShell module available in Royal TS V3 beta can be used to manage a Royal TS document. Thus it must be customized to meet specific needs, the script shows how to configure a couple of Remote Desktop connection properties as an example.
+    The script is meant to be scheduled, for example by using PowerShell Jobs or Scheduled Tasks, in order to have an updated Royal TS document based on active computer accounts in one or more specified Active Directory OU(s).
+    For smaller environments it may be appropriate to specify the domain DN as the root OU, but this is not recommended for larger environments. Instead the script may be run multiple times with different OU`s specified as the root OU.
+
+.Parameter RootOUPath
+	Specifies the path to the root OU for the OU structure you want to mirror
+	Example: 'OU=Servers,DC=lab,DC=local'
+
+.Parameter RoyalDocumentPath
+	Specifies the path to the Royal TS document you want to mirror the OU struture in. If the document does not exist, it will be created.
+	Example: 'C:\temp\Servers.rtsz'
+
+.Parameter RemoveInactiveComputerObjects
+	Switch parameter to specify whether you want to remove inactive computer objects from the Royal TS document
+
+.Parameter InactiveComputerObjectThresholdInDays
+	Specifies the number of days for the threshold defining inactive computer objects. If not specified a default value of 60 days will be used.
+
+.Parameter UpdateRoyalComputerProperties
+	Switch parameter to specify whether you want to update the Remote Desktop connection properties. Applies for computer objects both new and already present in the Royal TS document.
+    The properties to be updated is hard coded to enable Smart sizing and inheritance of credentials from the parent folder.
+
+.Parameter RTSPSModulePath
+	Specifies the path to the Royal TS PowerShell module. If Royal TS V3 beta was installed using the MSI-file, this parameter is not required.
+    Specify the path if you downloaded and extracted the zip-file version of Royal TS V3 to an alternate location.
+
+.Notes
+            Name: Update-RoyalFolder.ps1
+            Author: Jan Egil Ring
+            Date Created: 01 Jan 2015
+
+.Example
+	& C:\MyScripts\Update-RoyalFolder.ps1 -RootOUPath 'OU=Servers,DC=lab,DC=local' -RoyalDocumentPath C:\temp\Servers.rtsz
+	
+	Mirrors the OU structure in the C:\temp\Servers.rtsz Royal TS document based on computer accounts from the root OU OU=Servers,DC=lab,DC=local
+
+.Example
+	& C:\MyScripts\Update-RoyalFolder.ps1 -RootOUPath 'OU=Servers,DC=lab,DC=local' -RoyalDocumentPath C:\temp\Servers.rtsz -RemoveInactiveComputerObjects -UpdateRoyalComputerProperties -InactiveComputerObjectThresholdInDays 30
+	
+	Mirrors the OU structure in the C:\temp\Servers.rtsz Royal TS document based on computer accounts from the root OU OU=Servers,DC=lab,DC=local
+    Removes computer accounts already present in the Royal TS document folder which have not logged on to the domain for the last 10 days.
+    Enables Smart sizing and inheritance of credentials from the parent folder for existing objects if not already enabled.
+
+
+#>
+
+#requires -Version 4.0 -Module ActiveDirectory
+
+[CmdletBinding()]
+	param (
+		[Parameter(Mandatory = $true)]
+		[string]$RootOUPath,
+		[string]$RoyalDocumentPath = (Join-Path -Path $env:USERPROFILE -ChildPath ('Documents\' + $env:USERDOMAIN + '.rtsz')),
+		[switch]$RemoveInactiveComputerObjects,
+        [string]$InactiveComputerObjectThresholdInDays = '60',
+		[switch]$UpdateRoyalComputerProperties,
+        [string]$RTSPSModulePath = (Join-Path -Path ${env:ProgramFiles(x86)} -ChildPath 'code4ward.net\Royal TS V3\RoyalDocument.PowerShell.dll')
+	)
+
+
+# Verify that the Royal TS PowerShell module is present at the specified path.
+# In the beta version of Royal TS V3, the module is not available in $env:PSModulePath and thus needs to be imported explicitly
+if (-not (Test-Path -Path $RTSPSModulePath)) {
+
+throw "Royal TS PowerShell module does not exist at $RTSPSModulePath"
+
+}
+
+# Import the Royal TS PowerShell module
+if (-not (Get-Module -Name RoyalDocument.PowerShell)) {
+
+Import-Module $RTSPSModulePath
+
+}
+
+# Create a new RoyalStore in memory
+$Store = New-RoyalStore -UserName ($env:USERDOMAIN + '\' + $env:USERNAME)
+
+# Create the new document if it does not exist
+if (-not (Test-Path -Path $RoyalDocumentPath)) {
+
+$RoyalDocument = New-RoyalDocument -Name $env:USERDOMAIN -FileName $RoyalDocumentPath -Store $Store
+
+# Store the new document to disk and close it
+Out-RoyalDocument -Document $RoyalDocument
+Close-RoyalDocument -Document $RoyalDocument
+
+}
+
+# Open the Royal TS document
+$RoyalDocument = Open-RoyalDocument -FileName $RoyalDocumentPath -Store $Store
+
+# Define helper functions
+function Update-RoyalFolder ($SearchBase, $FolderName)
+{
+    
+$Date = Get-Date
+$ADObjects = Get-ADComputer -searchbase $SearchBase -SearchScope OneLevel -LDAPFilter "(&(objectCategory=computer)(operatingSystem=Windows Server*)(!serviceprincipalname=*MSClusterVirtualServer*))" -Properties description,lastlogondate | 
+Where-Object lastlogondate -gt $Date.AddDays(-$InactiveComputerObjectThresholdInDays) |  
+Select-Object -Property name,dnshostname,lastlogondate,description |
+Sort-Object -Property name
+
+if ($ADObjects) {
+
+Write-Verbose -Message "Updating $($FolderName)"
+
+$RoyalFolder = Get-RoyalObjects -Type Folder -Store $Store | Where-Object {$_.Name -eq $FolderName -and $_.Description -eq $SearchBase}
+
+$RDSConnectionNames = Get-RoyalObjects -Store $Store | 
+Where-Object {$_.objecttype -eq 'RoyalRDSConnection' -and $_.ParentId -eq $RoyalFolder.Id.Guid} | 
+Select-Object -ExpandProperty name
+
+$RDSConnections = Get-RoyalObjects -Store $Store | 
+Where-Object {$_.objecttype -eq 'RoyalRDSConnection' -and $_.ParentId -eq $RoyalFolder.Id.Guid} 
+
+
+foreach ($ADObject in $ADObjects) 
+{
+
+if ($ADObject.Name -notin $RDSConnectionNames) {
+
+    $RDS = New-RoyalObject -Folder $RoyalFolder -Type RoyalRDSConnection -Name $ADObject.Name -Description $ADObject.Description
+    $null = Set-RoyalObjectValue -Object $RDS -Property "URI" -Value $ADObject.DnsHostName
+
+    Write-Verbose -Message "Added $($ADObject.Name)"
+
+if ($UpdateRoyalComputerProperties) {
+   
+   Update-RoyalComputerProperty -ComputerObject $RDS
+   
+   }
+   
+
+    } else {
+    
+    if ($UpdateRoyalComputerProperties) {
+
+    Update-RoyalComputerProperty
+    
+    
+    }
+    
+    
+    }
+
+}
+
+} else {
+
+Write-Verbose -Message "No computer objects matching the selected filter found in OU $($FolderName)"
+
+}
+
+}
+
+
+function Test-RoyalFolder ($Description, $FolderName, $ParentFolderName, $ParentFolderNameDescription)
+{
+
+$RoyalFolder = Get-RoyalObjects -Type Folder -Store $Store | Where-Object {$_.Name -eq $FolderName -and $_.Description -eq $Description}
+
+
+if (-not ($RoyalFolder)) {
+
+if ($ParentFolderName) {
+
+$RoyalFolderParent = Get-RoyalObjects -Type Folder -Store $Store | Where-Object {$_.Name -eq $ParentFolderName -and $_.Description -eq $ParentFolderNameDescription}
+
+if (-not ($RoyalFolderParent)) {
+
+throw "Parent does not exists"
+
+}
+
+$null = New-RoyalObject -folder $RoyalFolderParent -Type RoyalFolder -Name $FolderName -Description $Description
+
+} else {
+
+$null = New-RoyalObject -folder $RoyalDocument -Type RoyalFolder -Name $FolderName -Description $Description
+
+}
+
+}
+
+}
+
+
+function Update-RoyalComputerProperty {
+
+param (
+$ComputerObject
+)
+
+if ($ComputerObject) {
+
+$item = $ComputerObject
+
+} else {
+
+$item = $RDSConnections  | 
+where-object {$_.ParentId -eq $RoyalFolder.Id.Guid -and $_.Name -eq $ADObject.Name}
+
+}
+
+Write-Verbose -Message "Checking for computer object $($item.Name) for property compliance"
+
+    if ($item.SmartSizing -ne $true) {
+
+    Write-Verbose -Message "Enabling SmartSizing for computer object $($item.Name)"
+
+    $item.Smartsizing = $true
+
+    }
+
+    if ($item.CredentialFromParent -ne $true) {
+
+    Write-Verbose -Message "Enabling CredentialFromParent for computer object $($item.Name)"
+
+    $item.CredentialFromParent = $true
+
+    }
+
+
+
+}
+
+
+try
+{
+    $RootOU = Get-ADOrganizationalUnit -Identity $RootOUPath -ErrorAction Stop
+}
+catch
+{
+
+    $error[0].Exception
+    throw "An error occured while retrieving the root OU"
+    
+    
+}
+
+
+Test-RoyalFolder -FolderName $RootOU.Name -Description $RootOU.DistinguishedName
+
+Update-RoyalFolder -SearchBase $RootOU.DistinguishedName -FolderName $RootOU.Name
+
+$OUs = Get-ADOrganizationalUnit -SearchBase $RootOU -Filter * -SearchScope OneLevel | Select-Object Name,DistinguishedName
+
+
+if ($OUs) {
+
+foreach ($OU in $OUs) {
+
+$ParentOU = $RootOU
+
+Test-RoyalFolder -FolderName $OU.Name -Description $OU.DistinguishedName -ParentFolderName $ParentOU.Name -ParentFolderNameDescription $ParentOU.DistinguishedName
+
+Update-RoyalFolder -SearchBase $OU.DistinguishedName -FolderName $OU.Name
+
+$ChildOUs = Get-ADOrganizationalUnit -SearchBase $OU.DistinguishedName -Filter * -SearchScope OneLevel | Select-Object Name,DistinguishedName
+
+$ChildOUsToProcess = $true
+
+while ($ChildOUsToProcess)
+{
+
+$ChildOUsToProcess = @()
+
+foreach ($ChildOU in $ChildOUs) {
+
+$ParentOU = $OU
+
+Test-RoyalFolder -FolderName $ChildOU.Name -Description $ChildOU.DistinguishedName -ParentFolderName $ParentOU.Name -ParentFolderNameDescription $ParentOU.DistinguishedName
+
+Update-RoyalFolder -SearchBase $ChildOU.DistinguishedName -FolderName $ChildOU.Name
+
+$ChildOUsToProcess += Get-ADOrganizationalUnit -SearchBase $ChildOU.DistinguishedName -Filter * -SearchScope OneLevel | Select-Object Name,DistinguishedName
+
+} #end foreach $ChildOU
+
+$ChildOUs = $ChildOUsToProcess
+
+
+    
+} #end while
+
+} #end foreach $OU
+
+} #end if $OUs
+    
+
+if ($RemoveInactiveComputerObjects) {
+
+Write-Verbose -Message "Checking for inactive computer objects to remove"
+
+$Date = Get-Date
+$ADObjects = Get-ADComputer -SearchBase $RootOUPath -LDAPFilter "(&(objectCategory=computer)(operatingSystem=Windows Server*)(!serviceprincipalname=*MSClusterVirtualServer*))" -Properties description,lastlogondate | 
+Where-Object lastlogondate -gt $Date.AddDays(-$InactiveComputerObjectThresholdInDays) |  
+Sort-Object name |
+Select-Object -ExpandProperty name
+
+
+$RoyalFolder = Get-RoyalObjects -Type Folder -Store $store | Where-Object {$_.Name -eq $RootOU.Name -and $_.Description -eq $RootOU.DistinguishedName}
+
+$RDSConnections = Get-RoyalObjects -Store $Store | 
+Where-Object {$_.objecttype -eq 'RoyalRDSConnection' -and $_.ParentId -eq $RoyalFolder.Id.Guid}
+
+foreach ($item in $RDSConnections)
+{
+    if ($item.Name -notin $ADObjects) {
+
+    Write-Verbose -Message "Removing inactive computer object $($item.Name)"
+
+    $null = Remove-RoyalObject -Object $item
+
+    }
+}
+
+
+
+
+}
+
+# Store the updated document to disk and close it
+Out-RoyalDocument -Document $RoyalDocument
+Close-RoyalDocument -Document $RoyalDocument
