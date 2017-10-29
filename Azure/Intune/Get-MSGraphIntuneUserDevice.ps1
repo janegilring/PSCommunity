e63afe84d4283de88f9e3e3092e1266cb0f6c583
Jan Egil Ring <janegilring@users.noreply.github.com>
2017-10-29 06:17:44 +0100

    Adds MSGraph Intune samples

---
+++ Azure/Intune/Get-MSGraphIntuneUserDevice.ps1
@@ -0,0 +1,44 @@
+﻿
+Function Get-MSGraphIntuneUserDevice {
+
+    <#
+.SYNOPSIS
+This function is used to get an AAD User Devices from the Graph API REST interface
+.DESCRIPTION
+The function connects to the Graph API Interface and gets a users devices registered with Intune MDM
+Created based on examples from https://github.com/microsoftgraph/powershell-intune-samples
+.EXAMPLE
+Get-MSGraphIntuneUserDevice -UserID $UserID
+Returns all user devices registered in Intune MDM
+.NOTES
+NAME: Get-MSGraphIntuneUserDevice
+#>
+
+    [cmdletbinding()]
+
+    param
+    (
+        [Parameter(Mandatory = $true, HelpMessage = "UserID (guid) for the user you want to take action on must be specified:")]
+        $UserID,
+        $AuthenticationToken
+    )
+
+    # Defining Variables
+    $graphApiVersion = "beta"
+    $Resource = "users/$UserID/managedDevices"
+
+    try {
+
+        $uri = "https://graph.microsoft.com/$graphApiVersion/$($Resource)"
+        Write-Verbose $uri
+        (Invoke-RestMethod -Uri $uri -Headers $AuthenticationToken -Method Get -ErrorAction Stop).Value
+
+    }
+
+    catch {
+
+        throw  $_.Exception.Message
+ 
+    }
+
+}
---
+++ Azure/Intune/Invoke-MSGraphIntuneDeviceAction.ps1
@@ -0,0 +1,120 @@
+﻿Function Invoke-MSGraphIntuneDeviceAction {
+
+    <#
+      .SYNOPSIS
+      This function is used to set a generic intune resources from the Graph API REST interface
+      .DESCRIPTION
+      The function connects to the Graph API Interface and sets a generic Intune Resource
+      Created based on examples from https://github.com/microsoftgraph/powershell-intune-samples
+      .EXAMPLE
+      Invoke-MSGraphIntuneDeviceAction -DeviceID $DeviceID -remoteLock
+      Resets a managed device passcode
+      .NOTES
+      NAME: Invoke-MSGraphIntuneDeviceAction
+  #>
+
+    [cmdletbinding()]
+
+    param
+    (
+        [Parameter(Mandatory = $true, HelpMessage = "Auth header must be specified:")]
+        [object]$AuthenticationToken,
+        [switch]$RemoteLock,
+        [switch]$ResetPasscode,
+        [switch]$RemoveCompanyData,
+        [switch]$FactoryReset,
+        [switch]$Reboot,
+        [Parameter(Mandatory = $true, HelpMessage = "DeviceId (guid) for the Device you want to take action on must be specified:")]
+        $DeviceID
+    )
+ 
+
+    $graphApiVersion = "Beta"
+
+    try {
+
+        $Count_Params = 0
+
+        if ($RemoteLock.IsPresent) { $Count_Params++ }
+        if ($ResetPasscode.IsPresent) { $Count_Params++ }
+        if ($RemoveCompanyData.IsPresent) { $Count_Params++ }
+        if ($factoryReset.IsPresent) { $Count_Params++ }
+        if ($Reboot.IsPresent) { $Count_Params++ }
+
+        if ($Count_Params -eq 0) {
+
+            write-host "No parameter set, specify -RemoteLock -ResetPasscode or -Wipe against the function" -f Red
+
+        }
+
+        elseif ($Count_Params -gt 1) {
+
+            write-host "Multiple parameters set, specify a single parameter -RemoteLock -ResetPasscode or -Wipe against the function" -f Red
+
+        }
+
+        elseif ($RemoteLock) {
+
+            $Resource = "managedDevices/$DeviceID/remoteLock"
+            $uri = "https://graph.microsoft.com/$graphApiVersion/$($resource)"
+            write-verbose $uri
+            Write-Verbose "Sending remoteLock command to $DeviceID"
+            Invoke-RestMethod -Uri $uri -Headers $AuthenticationToken -Method Post -ErrorAction Stop
+
+        }
+
+
+        elseif ($Reboot) {
+
+            $Resource = "managedDevices/$DeviceID/rebootNow"
+            $uri = "https://graph.microsoft.com/$graphApiVersion/$($resource)"
+            write-verbose $uri
+            Write-Verbose "Sending remoteLock command to $DeviceID"
+            Invoke-RestMethod -Uri $uri -Headers $AuthenticationToken -Method Post -ErrorAction Stop
+
+        }
+
+        elseif ($ResetPasscode) {
+
+
+            $Resource = "managedDevices/$DeviceID/resetPasscode"
+            $uri = "https://graph.microsoft.com/$graphApiVersion/$($resource)"
+            write-verbose $uri
+            Write-Verbose "Sending remotePasscode command to $DeviceID"
+            Invoke-RestMethod -Uri $uri -Headers $AuthenticationToken -Method Post -ErrorAction Stop
+
+
+        }
+
+        elseif ($RemoveCompanyData) {
+
+            $Resource = "managedDevices/$DeviceID/retire"
+            $uri = "https://graph.microsoft.com/$graphApiVersion/$($resource)"
+            write-verbose $uri
+            Write-Verbose "Sending removeCompanyData command to $DeviceID"
+            Invoke-RestMethod -Uri $uri -Headers $AuthenticationToken -Method Post -ErrorAction Stop
+
+
+        }
+
+        elseif ($factoryReset) {
+
+
+            $Resource = "managedDevices/$DeviceID/wipe"
+            $uri = "https://graph.microsoft.com/$graphApiVersion/$($resource)"
+            write-verbose $uri
+            Write-Verbose "Sending factoryReset command to $DeviceID"
+            Invoke-RestMethod -Uri $uri -Headers $AuthenticationToken -Method Post -ErrorAction Stop
+
+
+        }
+
+    }
+
+    catch {
+
+        throw  $_.Exception.Message
+
+    }
+
+}
